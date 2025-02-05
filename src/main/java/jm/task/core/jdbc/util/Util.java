8007c4c9d468a2287385c807269dcf9034aabe0a
Imwy345 <kostay2013.18@gmail.com>
2025-02-05 21:08:21 +0300

    init

--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -1,5 +1,20 @@
 package jm.task.core.jdbc.util;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class Util {
     // реализуйте настройку соеденения с БД
+    private static final String URL = "jdbc:postgresql://localhost:5432/task1";
+    private static final String USER = "postgres";
+    private static final String PASSWORD = "zs462282x";
+
+    public static Connection getConnection() {
+        try {
+            return DriverManager.getConnection(URL, USER, PASSWORD);
+        } catch (SQLException e) {
+            throw new RuntimeException("Ошибка подключения к базе данных", e);
+        }
+    }
 }

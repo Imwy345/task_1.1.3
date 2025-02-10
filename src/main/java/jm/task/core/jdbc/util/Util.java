20f37d0cb46cf7bd8d0f4ce8598b182a42a6a22a
Imwy345 <kostay2013.18@gmail.com>
2025-02-10 15:21:20 +0300

    initJDBC

--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -1,20 +1,35 @@
 package jm.task.core.jdbc.util;
 
+import jm.task.core.jdbc.dao.UserDaoJDBCImpl;
+
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.util.logging.Logger;
 
 public class Util {
     // реализуйте настройку соеденения с БД
+    private static final Logger logger = Logger.getLogger(Util.class.getName());
+    private static Connection connection;//Добавил поле connection
     private static final String URL = "jdbc:postgresql://localhost:5432/task1";
     private static final String USER = "postgres";
     private static final String PASSWORD = "zs462282x";
 
-    public static Connection getConnection() {
+
+    /*добавил реализацию подключения*/
+    private static void buildConnection() {
         try {
-            return DriverManager.getConnection(URL, USER, PASSWORD);
+            connection = DriverManager.getConnection(URL, USER, PASSWORD);
         } catch (SQLException e) {
-            throw new RuntimeException("Ошибка подключения к базе данных", e);
+            logger.severe("Ошибка подключения к базе данных");
         }
+    }
+
+    /*Добавил проверку*/
+    public static Connection getConnection() {
+        if (connection == null) {
+            buildConnection();
+        }
+        return connection;
     }
 }
